1913d5431bc6d2f6ca83fc92a162c9388f6bc2ef
mgarces <marc.garces@education.gouv.fr>
2020-03-11 09:00:54 +0100

    modif fiche

--- genericPages/pied.php
+++ genericPages/pied.php
@@ -1,6 +1,6 @@
 <table width=100% class="BH_CADRE">
     <tr>
-        <td with="30%"><small>V 1.01.00 </small></td>
+        <td with="30%"><small>V 2.00.00 </small></td>
         <td with="60%"><small></small></td>
         <td with="30%" style="text-align: right"><small><?=$infAppli['titre']?></small></td>
     </tr>
