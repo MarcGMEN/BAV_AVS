c940f514437d98b7da74199a7571f88244a6ee9f
SREN_MG <marc.garces@education.gouv.fr>
2022-04-12 17:26:12 +0200

    stat OK

--- pages/statSite.php
+++ pages/statSite.php
@@ -72,8 +72,8 @@
 				repr += "<div class='col-xs-6 col-sm-6 col-md-6' >" + index + "</div>";
 				repr += "<div class='col-xs-3 col-sm-3 col-md-3 link' onclick=\"inverseStat('" + idHtml + "')\" id='croix_" + idHtml + "'> + </div>";
 				repr += "<div class='col-xs-1 col-sm-1 col-md-1' >Cpt => " + tab['nb'] + "</div>";
-				repr += "<div class='col-xs-1 col-sm-1 col-md-1' onclick=\"dessin('" + idHtml + "',0)\">Jour</div>";
-				repr += "<div class='col-xs-1 col-sm-1 col-md-1' onclick=\"dessin('" + idHtml + "',1)\">Jour/Heure</div>";
+				repr += "<div class='col-xs-1 col-sm-1 col-md-1 link' onclick=\"initDate('" + idHtml + "');dessin('" + idHtml + "',0)\"><img src='Images/statBarre.png' height='20px'/>j</div>";
+				repr += "<div class='col-xs-1 col-sm-1 col-md-1 link' onclick=\"initDate('" + idHtml + "');dessin('" + idHtml + "',1)\"><img src='Images/statBarre.png' height='20px'/>j/h</div>";
 				repr += "</div>";
 				repr += "<div id='div_" + idHtml + "' style=\"display:none\">";
 				repr += tab['repr'];
@@ -114,8 +114,16 @@
 				repr += "</li>";
 
 				repr += "<div style='display:none;border:0px blue solid' id='divcanvas" + idHtml + "'>";
-				repr += "<canvas id='canvas" + idHtml + "' width='" + longueur + "' height='" + hauteur + "'>Votre navigateur est trop vieux</canvas>"
-				repr += "<span style='vertical-align:top' onclick=\"closeDesin('" + idHtml + "')\"> <img src='Images/erreur.png'/> </span>"
+				repr += "<div class='row'>";
+				repr += "<div class='col-xs-1 col-sm-1 col-md-1' ><input type='date' id='debut" + idHtml + "' onchange=\"redessin('" + idHtml + "')\"/></div>";
+				repr += "<div class='col-xs-10 col-sm-10 col-md-10' ></div>";
+				repr += "<div class='col-xs-1 col-sm-1 col-md-1' ><input type='date' id='fin" + idHtml + "'  onchange=\"redessin('" + idHtml + "')\"/></div>";
+				repr += "</div>";
+				repr += "<div class='row'>";
+				repr += "<div class='col-xs-11 col-sm-11 col-md-11' >";
+				repr += "<canvas id='canvas" + idHtml + "' width='" + longueur + "' height='" + hauteur + "'>Votre navigateur est trop vieux</canvas></div>"
+				repr += "<div class='col-xs-1 col-sm-1 col-md-1' style='vertical-align:top' onclick=\"closeDesin('" + idHtml + "')\"> <img src='Images/erreur.png'/> </div>";
+				repr += "</div>";
 				repr += "</div>";
 				nb += tab['nb'];
 
@@ -133,6 +141,10 @@
 		};
 	}
 
+	function redessin(id) {
+		dessin(id,hourG)
+	}
+
 	function closeDesin(id) {
 		var divCanvas = getElement("divcanvas" + id);
 		divCanvas.style.display = 'none';
@@ -145,32 +157,65 @@
 		
 
 	}
-	var longueur = screen.width * 0.7;
+	var longueur = window.innerWidth * 0.8;
 	// var longueur = 150;
 	var hauteur = 200;
-
-	function dessin(id, hour) {
-
-		var divCanvas = getElement("divcanvas" + id);
-		divCanvas.style.display = 'block';
-
-		var monCanvas = getElement("canvas" + id);
-		var ctx = monCanvas.getContext("2d");
-		ctx.font = "15px arial";
-
-
+	var hourG;
+	function initDate(id) {
 		// console.log("nb row " + nbRow);
 		//var nbRow = sizeof(tabDates[id]);
 		var start = null;
 		var max = 0
 		for (var date in tabDates[id]) {
-			var nb = tabDates[id][date];
 			if (start==null) {
 				start=new Date(date);
 				start.setMonth(start.getMonth()+1);
 			}
 			fin=new Date(date);
 			fin.setMonth(fin.getMonth()+1);
+		}
+		start.setDate(start.getDate()+1);
+		fin.setDate(fin.getDate()+1);
+
+		var debutHTML = getElement("debut" + id);
+		var finHTML = getElement("fin" + id);
+		debutHTML.value=start.toISOString().split('T')[0];
+		debutHTML.min=start.toISOString().split('T')[0];
+		debutHTML.max=fin.toISOString().split('T')[0];
+
+		finHTML.value=fin.toISOString().split('T')[0];
+		finHTML.min=start.toISOString().split('T')[0];
+		finHTML.max=fin.toISOString().split('T')[0];
+	}
+
+	function dessin(id, hour) {
+		closeDesin(id);
+		hourG=hour;
+		var divCanvas = getElement("divcanvas" + id);
+		divCanvas.style.display = 'block';
+
+		var monCanvas = getElement("canvas" + id);
+		var ctx = monCanvas.getContext("2d");
+		ctx.font = "15px arial";
+		
+		// console.log("nb row " + nbRow);
+		//var nbRow = sizeof(tabDates[id]);
+		var start = null;
+		
+		
+		
+		var debutHTML = getElement("debut" + id);
+		var finHTML = getElement("fin" + id);
+
+		start=new Date(debutHTML.value);
+		fin=new Date(finHTML.value);
+		
+		console.log(start);
+		console.log(fin);
+
+		var max = 0
+		for (var date in tabDates[id]) {
+			var nb = tabDates[id][date];
 			// console.log(date + " " + nb);
 			if (nb > max) {
 				max = nb;
@@ -179,12 +224,10 @@
 		console.log("max "+max);
 		console.log("nb "+nb);
 		fin.setDate(fin.getDate()+1);
-		console.log(start);
-		console.log(fin);
 
 		var nbRow = Math.floor((fin - start) / (60 * 60 * 1000)) + 1;
 		if (hour) {
-
+			
 		} else {
 			var nbRow = Math.floor((fin - start) / (24 * 60 * 60 * 1000)) + 1;
 		}
@@ -269,7 +312,11 @@
 				var cptHour = 0;
 			}
 			if (hour) {
-				if (cptHour++ % 6 == 0) {
+				mod=1;
+				if (largeur < 50) {
+					mod=6;
+				}
+				if (cptHour++ % mod == 0) {
 					ctx.beginPath();
 					ctx.moveTo(Xlettre, Ylettre);
 					ctx.font = "8px arial";
