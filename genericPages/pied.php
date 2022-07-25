d2f343bda7c57bf4adb3d6bdd146665de4a5d855
MGarces_dell <marc.garces@education.gouv.fr>
2022-07-25 22:08:19 +0200

    2022.08.00

--- genericPages/pied.php
+++ genericPages/pied.php
@@ -1,6 +1,6 @@
 <table width=100% class="BH_CADRE">
     <tr>
-        <td with="30%"><small>V 2022.11.00 </small></td>
+        <td with="30%"><small>V 2022.08.00 </small></td>
         <td with="60%"><small></small></td>
         <td with="30%" style="text-align: right"><small><?=$infAppli['titre']?></small></td>
     </tr>
