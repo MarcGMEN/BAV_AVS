47428888977e3748c1b8f9fd45bae1b9ce0c860a
MGarces_dell <marc.garces@education.gouv.fr>
2022-11-26 10:13:07 +0100

    alert +

--- JS/geoPosition.js
+++ JS/geoPosition.js
@@ -23,6 +23,7 @@ function initMap() {
     L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
         // Il est toujours bien de laisser le lien vers la source des données
         attribution: 'données © OpenStreetMap/ODbL - rendu OSM France',
+        fullscreenControl: true,
         minZoom: 1,
         maxZoom: 20
     }).addTo(macarte);
