32f234380a8e59d09af6f088820a4dbe60c1a3a7
MGarces_dell <marc.garces@education.gouv.fr>
2023-09-02 09:57:16 +0200

    venir

--- pages/venir.php
+++ pages/venir.php
@@ -50,7 +50,7 @@
 				<p><span class="label label-info"><img src="Images/iconeInstagram.png" height="20px"></span>
 					<span style="font-size: 16px;"><A href='https://www.instagram.com/bourse.aux.1000.velos/?hl=fr'  target='_blank'>@bourse.aux.1000.velos</A></span></p>
 				<p><span class="label label-info"><img src="Images/iconeFacebook.png" height="20px"></span>
-					<span style="font-size: 16px;"><A href='https://www.facebook.com/events/1127791811449916' target='_blank'>18eme Bourse aux 1000 vélos</A></span></p>
+					<span style="font-size: 16px;"><A href='https://fb.me/e/2OXOMDsqp' target='_blank'>19eme Bourse aux 1000 vélos</A></span></p>
 
 		</fieldset>
 	</div>
