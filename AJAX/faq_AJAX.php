4fb80ec8f85281f36365c0e1516738530e3b1f64
SREN_MG <marc.garces@education.gouv.fr>
2023-11-22 17:27:42 +0100

    faq

--- AJAX/faq_AJAX.php
+++ AJAX/faq_AJAX.php
@@ -15,7 +15,7 @@ function return_oneFaq($id)
 
 function action_createFaq($data)
 {
-   
+
 }
 
 function action_deleteFaq($id)
@@ -25,20 +25,23 @@ function action_deleteFaq($id)
 
 function action_updateFaq($data)
 {
-    $faq =tabToObject(string2Tab($data), "faq");
-        
+    $faq = tabToObject(string2Tab($data), "faq");
+
     try {
         updateFaq($faq);
         return $faq;
     } catch (Exception $e) {
-        return "ERREUR ".$e->getMessage();
+        return "ERREUR " . $e->getMessage();
     }
 }
 
 function action_insertFaq($obj)
 {
-    $tab =string2Tab($obj);
+    $tab = string2Tab($obj);
     insertFaq($tab);
+
+    sendMail("BAV FAQ", "bourse1000velos@avs44.com", "<html><body>Ajout d'un nouveau message dans la FAQ.<br/>" . addslashes(rtrim($tab['act_text'])) . "<body></html>");
+
     return true;
 }
 
