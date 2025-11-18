cc09a7c8fc95f0ddfdad3b96877fd3cc341f0eee
Marc Garcès <marc.garces@education.gouv.fr>
2025-11-18 15:44:08 +0100

    menu table

--- genericPages/menuTABLE.php
+++ genericPages/menuTABLE.php
@@ -1,15 +1,15 @@
 <div class="row" style="background-color: #0cd;font-size:1.1em">
-	<div class="col-md-12">
-		<div class="col-md-3 col-sm-3 col-xs-12" >
-			<div class="col-md-4 col-sm-4 col-xs-4" style="text-align:right">Pré-depot</div>
-			<div class="col-md-2 col-sm-2 col-xs-2">
+	<div class="col-md-12  col-menuTable">
+		<div class="col-md-3 col-sm-3 col-xs-12  col-menuTable" >
+			<div class="col-md-3 col-sm-3 col-xs-3 col-menuTableEtat">Dépôt</div>
+			<div class="col-md-3 col-sm-3 col-xs-3 col-menuTable">
 				<!-- fiche etat cofirme -->
 				<span id="e_CONFIRME" class='link' onclick='goTo("stock.php","obj_etat","CONFIRME",null)' style='font-weight: bold'>...</span>
 			</div>
-			<div class="col-md-3 col-sm-3 col-xs-3">
+			<div class="col-md-3 col-sm-3 col-xs-3 col-menuTable">
 				<div id=countModifData title="Nb de d'etiquette à imprimer"></div>
 			</div>
-			<div class="col-md-3 col-sm-3 col-xs-3">
+			<div class="col-md-3 col-sm-3 col-xs-3 col-menuTable" >
 				<div id=countModifVendeur title="Nb de coupons dépot à imprimer"></div>
 			</div>
 			<!-- <div class="col-md-3 col-sm-3 col-xs-2" style="text-align:right">Init</div>
@@ -18,37 +18,38 @@
 			</div> -->
 		</div>
 
-		<div class="col-md-3 col-sm-3 col-xs-12">
-			<div class="col-md-4 col-sm-4 col-xs-4" style="text-align:right">Stock</div>
-			<div class="col-md-2 col-sm-2 col-xs-2">
+		<div class="col-md-3 col-sm-3 col-xs-12 col-menuTable">
+			<div class="col-md-3 col-sm-3 col-xs-3 col-menuTableEtat" >Stock</div>
+			<div class="col-md-3 col-sm-3 col-xs-3 col-menuTable">
 				<!-- fiche etat cofirme -->
 				<span id="e_STOCK" class='link' onclick='goTo("stock.php","obj_etat","STOCK",null)' style='font-weight: bold'>...</span>
 			</div>
-			<div class="col-md-6 col-sm-6 col-xs-6">
-				<span id=countModifStock title="Nb de coupons sortie à imprimer"></span>
+			<div class="col-md-6 col-sm-6 col-xs-6 col-menuTable">
+				<div id=countModifStock title="Nb de coupons sortie à imprimer"></div>
 			</div>
 		</div>
-		<div class="col-md-2 col-sm-2 col-xs-12">
-			<div class="col-md-4 col-sm-4 col-xs-4" style="text-align:right">Vendu</div>
-			<div class="col-md-2 col-sm-2 col-xs-2">
+		<div class="col-md-3 col-sm-3 col-xs-12 col-menuTable">
+			<div class="col-md-3 col-sm-3 col-xs-3 col-menuTableEtat" >Vendu</div>
+			<div class="col-md-3 col-sm-3 col-xs-3 col-menuTable">
 				<!-- fiche etat cofirme -->
 				<span id="e_VENDU" class='link' onclick='goTo("stock.php","obj_etat","VENDU",null)' style='font-weight: bold'>...</span>
 			</div>
-			<div class="col-md-6 col-sm-6 col-xs-6">
-				<small><span id="statVendu">...</span></small>
-				<small><span title="Payé" class="PAYE link" id="PAYE" onclick='goTo("stock.php","obj_etat","PAYE",null)'></span></small>
+			<div class="col-md-6 col-sm-6 col-xs-6 col-menuTable">
+				<div><span id="statVendu">...</span>
+				<small><span title="Payé" class="PAYE link" id="PAYE" onclick='goTo("stock.php","obj_etat","PAYE",null)'>
+				</span></small></div>
 			</div>
 		</div>
-		<div class="col-md-2 col-sm-2 col-xs-12">
-			<div class="col-md-4 col-sm-4 col-xs-4" style="text-align:right">Rendu</div>
-			<div class="col-md-8 col-sm-8 col-xs-8">
+		<div class="col-md-2 col-sm-2 col-xs-12 col-menuTable">
+			<div class="col-md-6 col-sm-6 col-xs-6 col-menuTableEtat" >Rendu</div>
+			<div class="col-md-6 col-sm-6 col-xs-6 col-menuTable">
 				<span id="e_RENDU" class='link' onclick='goTo("stock.php","obj_etat","RENDU",null)' style='font-weight: bold'>...</span>
 				<small><span id="statRendu">...</span></small>
 			</div>
 		</div>
-		<div class="col-md-2 col-sm-2 col-xs-12">
-			<div class="col-md-4 col-sm-4 col-xs-4" style="text-align:right">Total</div>
-			<div class="col-md-8 col-sm-8 col-xs-8">
+		<div class="col-md-1 col-sm-1 col-xs-12 col-menuTable">
+			<div class="col-md-6 col-sm-6 col-xs-6 col-menuTableEtat" >TTC</div>
+			<div class="col-md-6 col-sm-6 col-xs-6 col-menuTable">
 				<!-- fiche etat valide -->
 				<span id="e_TOTAL" style='font-weight: bold'>...</span>
 			</div>
