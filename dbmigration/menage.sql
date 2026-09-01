1ab4431d11586418bc35213c6288bf794d8fa0c1
Marc garcès <marc.garces@education.gouv.fr>
2026-09-01 14:49:27 +0200

    svg base

--- dbmigration/menage.sql
+++ dbmigration/menage.sql
@@ -2,12 +2,12 @@
 -- recherche des clients sans fiche
 SELECT cli_nom, cli_emel FROM `bav_client` 
 where not exists (select obj_id_vendeur from bav_objet where obj_id_vendeur = cli_id OR obj_id_acheteur = cli_id )
-and cli_id > 15
+and cli_id > 30
 
 -- suppression des clients sans fiche
 delete from  `bav_client` 
 where not exists (select obj_id_vendeur from bav_objet where obj_id_vendeur = cli_id OR obj_id_acheteur = cli_id )
-and cli_id > 15;
+and cli_id > 30;
 
 
 update bav_objet set obj_type = 'Route' where obj_type  ='Demi-course';  
@@ -18,16 +18,36 @@ ALTER TABLE bav.bav_objet MODIFY COLUMN obj_type enum('Autre','Route','Gravel','
 ALTER TABLE bav.bav_objet MODIFY COLUMN obj_public enum('Autre','Homme','Femme','Mixte','Garçon','Fille') CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT 'Autre' NULL;
 
 
-INSERT INTO bav_client (cli_id, cli_id_modif, cli_nom, cli_emel, cli_adresse, cli_adresse1, cli_code_postal, cli_ville, cli_telephone, cli_telephone_bis, cli_taux_com, cli_prix_depot) VALUES(1, '4040e35284bdf5405cb3070134d900a7', 'Vendeur 2019', 'bourseauxvelosV2019@avs.com', '', '', '', '', '', '', 10.00, 3.00);
-INSERT INTO bav_client (cli_id, cli_id_modif, cli_nom, cli_emel, cli_adresse, cli_adresse1, cli_code_postal, cli_ville, cli_telephone, cli_telephone_bis, cli_taux_com, cli_prix_depot) VALUES(2, '2eef1be7ce272b7089400619618b1637', 'Acheteur 2019', 'bourseauxvelosA2019@avs.com', '', '', '', '', '', '', 10.00, 3.00);
-INSERT INTO bav_client (cli_id, cli_id_modif, cli_nom, cli_emel, cli_adresse, cli_adresse1, cli_code_postal, cli_ville, cli_telephone, cli_telephone_bis, cli_taux_com, cli_prix_depot) VALUES(3, '52df4bc606eaddf149390506ab8cd73f', 'Vendeur 2021', 'bourseauxvelosV2021@avs.com', '', '', '', '', '', '', 10.00, 3.00);
-INSERT INTO bav_client (cli_id, cli_id_modif, cli_nom, cli_emel, cli_adresse, cli_adresse1, cli_code_postal, cli_ville, cli_telephone, cli_telephone_bis, cli_taux_com, cli_prix_depot) VALUES(4, '0b894c49a867df1b790d8d8c37bdf16e', 'Acheteur 2021', 'bourseauxvelosA2021@avs.com', '', '', '', '', '', '', 10.00, 3.00);
-INSERT INTO bav_client (cli_id, cli_id_modif, cli_nom, cli_emel, cli_adresse, cli_adresse1, cli_code_postal, cli_ville, cli_telephone, cli_telephone_bis, cli_taux_com, cli_prix_depot) VALUES(5, '1d9dc5d01174efa146031b68a38761aa', 'Vendeur 2022', 'bourseauxvelosV2022@avs.com', '', '', '', '', '', '', 10.00, 3.00);
-INSERT INTO bav_client (cli_id, cli_id_modif, cli_nom, cli_emel, cli_adresse, cli_adresse1, cli_code_postal, cli_ville, cli_telephone, cli_telephone_bis, cli_taux_com, cli_prix_depot) VALUES(6, 'a1ca58ffe9a3f0118dc85ec596229079', 'Acheteur 2022', 'bourseauxvelosA2022@avs.com', '', '', '', '', '', '', 10.00, 3.00);
+INSERT INTO bav_client (cli_id, cli_id_modif, cli_nom, cli_emel, cli_adresse, cli_adresse1, cli_code_postal, cli_ville, cli_telephone, cli_telephone_bis, cli_taux_com, cli_prix_depot) 
+    VALUES(1, '4040e35284bdf5405cb3070134d900a7', 'Vendeur 2019', 'bourseauxvelosV2019@avs.com', '', '', '', '', '', '', 10.00, 3.00);
+INSERT INTO bav_client (cli_id, cli_id_modif, cli_nom, cli_emel, cli_adresse, cli_adresse1, cli_code_postal, cli_ville, cli_telephone, cli_telephone_bis, cli_taux_com, cli_prix_depot) 
+    VALUES(2, '2eef1be7ce272b7089400619618b1637', 'Acheteur 2019', 'bourseauxvelosA2019@avs.com', '', '', '', '', '', '', 10.00, 3.00);
+INSERT INTO bav_client (cli_id, cli_id_modif, cli_nom, cli_emel, cli_adresse, cli_adresse1, cli_code_postal, cli_ville, cli_telephone, cli_telephone_bis, cli_taux_com, cli_prix_depot) 
+    VALUES(3, '52df4bc606eaddf149390506ab8cd73f', 'Vendeur 2021', 'bourseauxvelosV2021@avs.com', '', '', '', '', '', '', 10.00, 3.00);
+INSERT INTO bav_client (cli_id, cli_id_modif, cli_nom, cli_emel, cli_adresse, cli_adresse1, cli_code_postal, cli_ville, cli_telephone, cli_telephone_bis, cli_taux_com, cli_prix_depot) 
+    VALUES(4, '0b894c49a867df1b790d8d8c37bdf16e', 'Acheteur 2021', 'bourseauxvelosA2021@avs.com', '', '', '', '', '', '', 10.00, 3.00);
+INSERT INTO bav_client (cli_id, cli_id_modif, cli_nom, cli_emel, cli_adresse, cli_adresse1, cli_code_postal, cli_ville, cli_telephone, cli_telephone_bis, cli_taux_com, cli_prix_depot) 
+    VALUES(5, '1d9dc5d01174efa146031b68a38761aa', 'Vendeur 2022', 'bourseauxvelosV2022@avs.com', '', '', '', '', '', '', 10.00, 3.00);
+INSERT INTO bav_client (cli_id, cli_id_modif, cli_nom, cli_emel, cli_adresse, cli_adresse1, cli_code_postal, cli_ville, cli_telephone, cli_telephone_bis, cli_taux_com, cli_prix_depot) 
+    VALUES(6, 'a1ca58ffe9a3f0118dc85ec596229079', 'Acheteur 2022', 'bourseauxvelosA2022@avs.com', '', '', '', '', '', '', 10.00, 3.00);
 -- => OK le 24/11/2022 
 
-INSERT INTO bav_client (cli_id, cli_id_modif, cli_nom, cli_emel, cli_adresse, cli_adresse1, cli_code_postal, cli_ville, cli_telephone, cli_telephone_bis, cli_taux_com, cli_prix_depot) VALUES(7 '1d9dc5d01174efa148031b68a38761ab', 'Vendeur 2023', 'bourseauxvelosV2023@avs.com', '', '', '', '', '', '', 10.00, 3.00);
-INSERT INTO bav_client (cli_id, cli_id_modif, cli_nom, cli_emel, cli_adresse, cli_adresse1, cli_code_postal, cli_ville, cli_telephone, cli_telephone_bis, cli_taux_com, cli_prix_depot) VALUES(8, 'a1ca58ffe9a3f0117dc85ec59622907b', 'Acheteur 2023', 'bourseauxvelosA2023@avs.com', '', '', '', '', '', '', 10.00, 3.00);
+INSERT INTO bav_client (cli_id, cli_id_modif, cli_nom, cli_emel, cli_adresse, cli_adresse1, cli_code_postal, cli_ville, cli_telephone, cli_telephone_bis, cli_taux_com, cli_prix_depot) 
+    VALUES(7 '1d9dc5d01174efa148031b68a38761ab', 'Vendeur 2023', 'bourseauxvelosV2023@avs.com', '', '', '', '', '', '', 10.00, 3.00);
+INSERT INTO bav_client (cli_id, cli_id_modif, cli_nom, cli_emel, cli_adresse, cli_adresse1, cli_code_postal, cli_ville, cli_telephone, cli_telephone_bis, cli_taux_com, cli_prix_depot) 
+    VALUES(8, 'a1ca58ffe9a3f0117dc85ec59622907b', 'Acheteur 2023', 'bourseauxvelosA2023@avs.com', '', '', '', '', '', '', 10.00, 3.00);
+
+INSERT INTO bav_client (cli_id, cli_id_modif, cli_nom, cli_emel, cli_adresse, cli_adresse1, cli_code_postal, cli_ville, cli_telephone, cli_telephone_bis, cli_taux_com, cli_prix_depot) 
+    VALUES(9 '74cd006ce37f8c4e72dc64034943a730', 'Vendeur 2024', 'bourseauxvelosV2024@avs.com', '', '', '', '', '', '', 10.00, 4.00);
+INSERT INTO bav_client (cli_id, cli_id_modif, cli_nom, cli_emel, cli_adresse, cli_adresse1, cli_code_postal, cli_ville, cli_telephone, cli_telephone_bis, cli_taux_com, cli_prix_depot) 
+    VALUES(10, '54930284891fe633bac51405ef285d1b', 'Acheteur 2024', 'bourseauxvelosA2024@avs.com', '', '', '', '', '', '', 10.00, 4.00);
+
+INSERT INTO bav_client (cli_id, cli_id_modif, cli_nom, cli_emel, cli_adresse, cli_adresse1, cli_code_postal, cli_ville, cli_telephone, 
+    cli_telephone_bis, cli_taux_com, cli_prix_depot, cli_id_cre) 
+    VALUES(11, '84cd006ce37f8c4e72dc64034943a730', 'Vendeur 2025', 'bourseauxvelosV2025@avs.com', '', '', '', '', '', '', 10.00, 4.00,0);
+INSERT INTO bav_client (cli_id, cli_id_modif, cli_nom, cli_emel, cli_adresse, cli_adresse1, cli_code_postal, cli_ville, cli_telephone, 
+    cli_telephone_bis, cli_taux_com, cli_prix_depot, cli_id_cre) 
+    VALUES(12, '64930284891fe633bac51405ef285d1b', 'Acheteur 2025', 'bourseauxvelosA2025@avs.com', '', '', '', '', '', '', 10.00, 4.00,0);
 
 
 
@@ -106,4 +126,24 @@ where obj_numero_bav = '2024';
 update bav_objet 
 set obj_id_acheteur = 10     
 where obj_numero_bav = '2024' 
+and (obj_id_acheteur is not null or obj_id_acheteur != 0) and obj_etat in ('PAYE','VENDU')  ;
+
+
+-- purge des clients 2025 : OK le 01/09/2025    
+select  count(*) from bav_objet 
+where obj_numero_bav = '2025';
+--1070
+
+select  count(*) from bav_objet 
+where obj_numero_bav = '2025'
+and (obj_id_acheteur is not null or obj_id_acheteur != 0) and obj_etat in ('PAYE','VENDU')  ;
+--723
+
+update bav_objet 
+set obj_id_vendeur = 11
+where obj_numero_bav = '2025';
+
+update bav_objet 
+set obj_id_acheteur = 12     
+where obj_numero_bav = '2025' 
 and (obj_id_acheteur is not null or obj_id_acheteur != 0) and obj_etat in ('PAYE','VENDU')  ;
